c1ab34b74e67108cfbe6434b579bb6d5d9ce53b1
testworldweb <testworldwideweb@gmail.com>
2016-07-05 18:50:57 -0700

    My uploaded books- Book id

--- app/controllers/edit_book_controller.rb
+++ app/controllers/edit_book_controller.rb
@@ -30,7 +30,7 @@ class EditBookController < ApplicationController
     book_uid = params[:book_uid]
     
     if (book_uid)
-      book = Book.where(:uid => book_uid, :library_id => current_library.id).first
+      book = Book.where(:uid => book_uid).first
       book_id = session[:book_id] = book.id.to_s if book
     elsif (book_id)
       session[:book_id] = book_id.strip
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 get "upload_book/upload"
  post "upload_book/submit"
- 
+   get "edit_book/edit"
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
